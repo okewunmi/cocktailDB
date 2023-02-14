eae56619ba3c028dcac727987766e279a6c2321f
okewunmi <okewunmiafeezolaide@gmail.com>
2023-02-14 22:17:41 +0100

    state commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        A town hall different from bala blu, blue blu bulaba. broom broom broom
+        brooooooooom. Bala blu blue blu bulaba. The farmers will make more
+        money. Your lunch will not be imported, cassava garri ewa and ehhh
+        ehhhhnn. The farmer will make money, the dinner would be cassava, eba,
+        ewa and everything.
+      </p>
+    </section>
+  );
+};
+
+export default About;
